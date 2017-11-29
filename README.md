fc4d83002ab95b01d3c015074d79bc46eb5b17a2
lempiy <lempiyada@gmail.com>
2017-11-29 18:21:55 +0200

    Init backend

---
+++ README.md
@@ -0,0 +1,3 @@
+## Annotation Tool v2.0
+
+New annotaion tool for camera project based on SLIC, OPENCV, WebAssembly. Golang version.
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/kurige/SLIC"
+	"image"
+	"os"
+	"image/png"
+	"flag"
+	"runtime"
+	"fmt"
+)
+
+func writePNG(img image.Image, filename string) {
+	fi, _ := os.Create(filename)
+	png.Encode(fi, img)
+}
+
+func main() {
+	flag.Parse()
+
+	nc := runtime.NumCPU()
+	runtime.GOMAXPROCS(nc)
+
+	superpixels := 200 // number of superpixels
+	compactness := 20.0 // compactness ratio (between distance and color)
+	iterations := 10 // number of slic iteration
+
+
+	file_name := flag.Arg(0)
+	file, err := os.Open(file_name)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	src_img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println(err, "Could not decode image:", file_name)
+		return
+	}
+
+	w := src_img.Bounds().Size().X
+	h := src_img.Bounds().Size().Y
+	dst := image.NewRGBA(
+		image.Rectangle{
+			image.Point{X: 0, Y: 0},
+			image.Point{X: w, Y: h},
+			})
+
+	superpixelsize := slic.SuperPixelSizeForCount(w, h, superpixels)
+
+	s := slic.MakeSlic(src_img, compactness, superpixelsize)
+	s.Run(iterations)
+
+	writePNG(s.DrawEdgesToImage(dst), "out.png")
+}
